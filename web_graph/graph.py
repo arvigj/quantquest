325ded71fd623d8fa7824a45427dbbe1f50fe809
rishabhranawat <rishabhranawat12345@gmail.com>
2016-04-12 15:41:56 -0400

    updates

--- web_graph/graph.py
+++ web_graph/graph.py
@@ -1,7 +1,8 @@
 #graph template file
-
+from spacy.en import *
 
 class Node:
+    nlp = English()
     def __init__(self,name):
         self.name = name
         self.link_list = {}
@@ -19,6 +20,18 @@ class Node:
             for j in neighbor.link_list.keys():
                 if i==j:
                     self.neighbors[neighbor].append(i)
+
+    def add_neighbor_industry(self,neighbor,neighbor_list):
+        if neighbor in self.neighbors:
+            return
+        self.neighbors[neighbor] = []
+        for i in self.link_list.keys():
+            for j in neighbor.link_list.keys():
+                c1 = self.nlp(unicode(i))
+                c2 = self.nlp(unicode(j))
+                if (c1.similarity(c2)) > 0.5:
+                    self.neighbors[neighbor].append(i)
+    
     def get_neighbor_links(self,neighbor):
         return self.neighbors[neighbor]
 
@@ -36,6 +49,14 @@ class Graph:
                     continue
                 else:
                     i.add_neighbor(j,j.link_list)
+    def link_all_industry(self):
+        for i in self.comp_list:
+            for j in self.comp_list:
+                if i==j:
+                    continue
+                else:
+                    i.add_neighbor_industry(j,j.link_list)
+
     def return_links(self,node_1, node_2):
         n2 = 0
         for i in self.comp_list:
--- web_graph/link_industries.py
+++ web_graph/link_industries.py
@@ -3,9 +3,6 @@ from pprint import pprint
 import sys
 import numpy as np
 
-from tsne import tsne
-#from pylab import Plot
-
 from graph import Node, Graph
 
 matrix = []
@@ -23,7 +20,7 @@ g = Graph()
 for company in data.keys()[0:num]:
     g.add_nodes(company,data[company])
 
-g.link_all_nodes()
+g.link_all_industry()
 
 
 for i in xrange(0,num):
@@ -35,7 +32,7 @@ for i in xrange(0,num):
             a = []
         matrix[i].append(len(a))
 
-mat = np.array(matrix,dtype=float)
+mat = np.matrix(matrix,dtype=float)
 
 #print type(mat)
 #print type(mat[0])
@@ -44,7 +41,7 @@ mat = np.array(matrix,dtype=float)
 for i in xrange(0,num):
     for j in xrange(0,num):
         if i!=j:
-            mat[i][j] /= mat[i][i]
+            mat[i,j] /= mat[i,i]
 
 #remove 1's before normalization
 #for i in xrange(0,num):
@@ -57,27 +54,18 @@ def norm(array, identity_index, arr_len):
     for i in xrange(0,arr_len):
         if i == identity_index:
             continue
-        length += array[i]
+        length += array[0,i]
     for i in xrange(0,arr_len):
         if i == identity_index:
             continue
-        array[i] = array[i]/length
+        array[0,i] = array[0,i]/length
     return array
 
 
 
 #insert 1's after normalization
 for i in xrange(0,num):
-    mat[i][i] = 1
+    mat[i,i] = 1
     mat[i] = norm(mat[i],i,num)
 
-
-
 print(np.round(mat,3))
-
-
-Y = tsne(mat, 2, num, 20.0)
-print len(Y[:,0])
-print len(Y[:,1])
-Plot.scatter(Y[:,0], Y[:,1], 20, labels)
-Plot.show()
